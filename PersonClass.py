50ec5aad6421ef9e7527ffd4aaa2130c23d1d09d
tgorn <34916537+tgorn@users.noreply.github.com>
2020-03-28 11:44:13 +0100

    Create PersonClass.py
    Person Class test

---
+++ PersonClass.py
@@ -0,0 +1,13 @@
+from Person_mod.Class_01 import Person
+
+
+
+a = Person("Tomek", 45)
+b = Person("Karolina", 42)
+c = Person("Krzysztof", 18)
+d = Person("Iga", 17)
+e = Person("Ryszard", 6)
+
+a.myprint()
+
+dict_family = {}
